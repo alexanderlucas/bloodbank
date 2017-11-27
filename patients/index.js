c7164143b59ffcb4d33d202fe158a7da4c7609d9
Alex Lucas <alexander.lucas@case.edu>
2017-11-26 21:13:13 -0500

    removed node request

--- patients/index.js
+++ patients/index.js
@@ -2,20 +2,7 @@ function displayPatient(patientButton) {
     alert('hi');
     var name = patientButton.id;
 
-    var con = mysql.createConnection({
-        host: "http://eecs341finalproject.clsvctncuryj.us-east-2.rds.amazonaws.com",
-        user: "eecs341",
-        password: "eecs341bloodbank",
-        database: "bloodbank"
-    });
 
-    con.connect(function(err) {
-        if (err) throw err;
-        con.query("SELECT * FROM customers", function(err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-        });
-    });
     alert(name);
 
 }
